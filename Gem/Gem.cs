deed36adb1e49817b99a52e37bd1dbbed2c354f2
Andrey Ribeiro <andrey.grigoletto2@gmail.com>
2025-04-06 11:49:37 -0300

    Paddle and gem now both move. Paddle can move with input of A and D

---
+++ Gem/Gem.cs
@@ -0,0 +1,18 @@
+using Godot;
+using System;
+
+public partial class Gem : Area2D
+{
+	[Export] float _speed = 110.0f;
+
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+		Position += new Vector2(0, _speed * (float)delta);
+	}
+}
