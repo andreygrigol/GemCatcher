8fdee4bb3695b4d64ec856a0ac511c5bb1747d14
Andrey Ribeiro <andrey.grigoletto2@gmail.com>
2025-04-07 11:42:15 -0300

    Added new signals

--- Gem/Gem.cs
+++ Gem/Gem.cs
@@ -4,6 +4,7 @@ using System;
 public partial class Gem : Area2D
 {
 	[Export] float _speed = 110.0f;
+	[Signal] public delegate void OnScoredEventHandler();
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
@@ -19,6 +20,7 @@ public partial class Gem : Area2D
 
 	private void OnAreaEntered(Area2D area){
 		GD.Print("Scored!");
+		EmitSignal(SignalName.OnScored);
 		QueueFree();
 	}
 }
